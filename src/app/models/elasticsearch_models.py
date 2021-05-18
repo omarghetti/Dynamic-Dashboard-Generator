0fae339cb610b4b923aedd27b16915c0ca5d2a88
Omar Ghetti <omarghetti2@gmail.com>
2021-05-18 17:48:01 +0200

    Added ES models to create queries

--- src/app/models/elasticsearch_models.py
+++ src/app/models/elasticsearch_models.py
@@ -1,1 +1,39 @@
+import os
 
+
+class ESAggregations():
+  def __init__(self, field, value):
+    self.field = field
+    self.value = value
+
+    class Meta:
+      connection_alias = os.environ.get('APP_NAME')
+
+
+class ESMetric():
+  def __init__(self, value, operator, field):
+    self.value = value
+    self.field = field
+    self.operator = operator
+
+    class Meta:
+      connection_alias = os.environ.get('APP_NAME')
+
+
+class DateHistogram():
+  def __init__(self, interval):
+    self.interval = interval
+
+    class Meta:
+      connection_alias = os.environ.get('APP_NAME')
+
+
+class ESquery():
+  def __init__(self, metrics, date_histogram, aggregations, select):
+    self.metrics = metrics
+    self.date_histogram = date_histogram
+    self.aggregations = aggregations
+    self.select = select
+
+    class Meta:
+      connection_alias = os.environ.get('APP_NAME')
